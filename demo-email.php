c6da54cd22227e5faefda098207e865f7348699f
Mubashar Ahmad <mubashardev0203@gmail.com>
2024-12-24 22:43:07 +0500

    Update Email design

---
+++ demo-email.php
@@ -0,0 +1,55 @@
+<?php
+$first_name = "wasim akram";
+  $backgroundImageUrl = 'https://mrbtc.co.uk/images/coming_soon.jpg';
+               $body =' 
+                <section style="margin: -8px;">
+  <div style="width: 100%; max-width: 730px; margin: 0 auto;">
+    <div id="content_div" style="background-color: #000;">
+      <div class="image-delay" style="width: 100%">
+        <img src="images/coming_soon.jpg" alt="" width="100%">
+      </div>
+      <div style="background: linear-gradient(180deg, rgb(34 24 46) 0%, rgba(0, 0, 0, 1) 20%); color: #fff;">
+        <div class="content-btn" style="width: 100%; padding: 12px 0px 20px; margin: 0 auto; text-align: center">
+          <br><br>
+          <a href="https://t.me/+7Zgdh6YVt4Q2Y2Y8"
+            style="text-decoration: none; padding: 10px 20px; border-radius: 40px; background: linear-gradient(90deg, rgba(139,84,233,1) 0%, #25f2b1 100%); color: #fff; width: fit-content; margin: 0 auto; font-size: 26px; font-family: sans-serif !important;"
+            class="link-btn"><strong>START NOW</strong></a>
+          <br>
+          <br><br>
+          <span style="text-align: center; font-family: sans-serif; font-weight: 500; font-size: 30px; ">
+            <span style="color: #3687ff">Crypto Made Clear,</span> <br><span style="color: #8b54e9;">Success Made
+              Simple!</span></span>
+        </div>
+        <div class="content-body"
+          style="padding: 10px 30px 100px 30px; text-align: left; font-family: sans-serif; font-weight: 500; font-size: 18px;">
+          Dear ' . $first_name . ',
+          <br><br>
+          Thank you for joining Crypto Joe! We\'re excited to have you on board and to be part of your journey into the
+          world
+          of cryptocurrency. Whether you\'re a complete beginner or looking to expand your knowledge, we\'re here to
+          help
+          you
+          every step of the way.
+          <br><br>
+          At Crypto Joe, we are dedicated to providing you with clear, easy-to-understand education to help you navigate
+          the
+          exciting world of crypto. We believe everyone can succeed with the right resources and guidance, and we\'re
+          thrilled to be part of your learning experience.
+          <br><br>
+          To get started, join our Telegram group where you can connect with other members, ask questions, and receive
+          updates. Click the link below to join:
+          <br>
+          [Telegram Link]
+          <br><br>
+          We look forward to supporting you in your crypto journey!
+          <br><br>
+          Best regards,
+          <br>
+          The Crypto Joe Team
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+            ';
+ ?>
